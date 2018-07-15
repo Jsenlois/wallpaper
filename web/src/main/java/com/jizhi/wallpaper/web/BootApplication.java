647531cdda9332312e21a299650e5c95a57be8b1
JsenLois <799699730@qq.com>
2018-07-15 17:32:20 +0800

    logback

--- web/src/main/java/com/jizhi/wallpaper/web/BootApplication.java
+++ web/src/main/java/com/jizhi/wallpaper/web/BootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.jizhi.wallpaper"})
 public class BootApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class);
     }
